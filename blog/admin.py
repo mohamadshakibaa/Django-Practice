e709333844c410080f8f9dfe14119fcd9bbf79b7
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-06-07 12:22:22 +0330

    Add a new model its name is Category

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 #@admin.register(Post)     ham in doroste hm paeini
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty'
-    list_display = ('title','author',  'created_date', 'status', 'counted_view', 'published_date')
+    list_display = ('title', 'author',  'created_date', 'status', 'counted_view', 'published_date')
     list_filter = ('status','author')
     search_fields = ['title', 'content']
     
 
-
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin) #-ham in doroste hm decorator
