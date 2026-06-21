3a6aca6b294715119cd39c755e09359dacbeb4cb
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-06-21 14:53:45 +0330

    with 'Inclusion template tag' create us Post categories list

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -17,3 +17,13 @@ def function():
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for category in categories:
+        cat_dict[category] = posts.filter(category=category).count()
+    
+    return {'categories': cat_dict} 
