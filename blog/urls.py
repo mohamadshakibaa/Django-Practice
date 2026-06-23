6202d5f0a31278f95f756703298980ce6ef714ab
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-06-23 11:40:28 +0330

    add a part about author and create url and add this to blog-view also use author__usename for give its id

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', blog_view , name='blog-home'),
     path('<int:pid>/', blog_single, name='single'),
     path('category/<str:cat_name>/', blog_view, name='category'),
+    path('author/<str:author_username>/', blog_view, name='author'),
     path('test1', test1, name='test1')
     # path('<int:pid>/', test, name= 'test')
 ]
