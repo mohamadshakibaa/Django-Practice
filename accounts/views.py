3a462f6c1b3e5c641550c7fb1760e498c67104b6
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-06-29 21:33:44 +0330

    Add authentication and create a new Bootstrap login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_view(request):
@@ -7,6 +8,13 @@ def login_view(request):
 #     else:
 #         msg = 'user is not attenticated '
 #                                                 , {'msg': msg}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return  redirect('/')
     return render(request, 'accounts/login.html')
 
 def logout_view(request):
