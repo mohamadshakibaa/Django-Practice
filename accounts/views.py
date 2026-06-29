43c38d9f4a9973e396f73c82379ee0d32e870097
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-06-29 23:20:26 +0330

    Add AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def login_view(request):
 #     if request.user.is_authenticated:
@@ -8,14 +8,22 @@ def login_view(request):
 #     else:
 #         msg = 'user is not attenticated '
 #                                                 , {'msg': msg}
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return  redirect('/')
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid(): 
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return  redirect('/')
+        
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
 
 def logout_view(request):
     pass
