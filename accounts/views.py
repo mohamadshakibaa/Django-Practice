61c83995ea6642106784a8049700711d7462a08b
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-06-30 02:12:33 +0330

    Add Signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 def login_view(request):
 #     if request.user.is_authenticated:
@@ -32,4 +33,16 @@ def logout_view(request):
     return redirect('/')
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)       # this part very important for your signup
+                return redirect('/')
+            
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
