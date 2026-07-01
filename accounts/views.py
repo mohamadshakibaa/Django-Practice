e35f3e1f0f62dffb0adcaf23b28b53df2a51347b
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-07-01 18:56:35 +0330

    Add login with email or username , both of them are True

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.http import HttpResponse
 # Create your views here.
 def login_view(request):
 #     if request.user.is_authenticated:
@@ -10,30 +12,34 @@ def login_view(request):
 #     else:
 #         msg = 'user is not attenticated '
 #                                                 , {'msg': msg}
-    if not request.user.is_authenticated:
-        if request.method == 'POST':
-            form = AuthenticationForm(request=request, data=request.POST)
-            if form.is_valid(): 
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                user = authenticate(request, username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    next_url = request.POST.get('next')
-                    if not next_url or next_url == "None":
-                        next_url = "/"
-
-                    return redirect(next_url)
-                        
-        form = AuthenticationForm()
-        context = {'form': form, 'next': request.GET.get('next')}
-        return render(request, 'accounts/login.html', context)
-    else:
+    if request.user.is_authenticated:
         next_url = request.GET.get('next')
         if next_url:
             return redirect(next_url)
-
         return redirect('/')
+    
+    if request.method == 'POST':
+        data = request.POST.copy()
+        if '@' in data['username']:
+            user = User.objects.filter(email=data['username']).first()
+            if user:
+                data['username'] = user.username
+        form = AuthenticationForm(request=request, data=data)
+        
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            next_url = request.POST.get('next')
+            if not next_url or next_url == "None":
+                next_url = "/"
+            return redirect(next_url)
+        
+    else:               
+        form = AuthenticationForm()
+    
+    context = {'form': form, 'next': request.GET.get('next')}
+    return render(request, 'accounts/login.html', context)
+
 
 @login_required
 def logout_view(request):
